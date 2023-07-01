810ea199d621dee5e4962f1c3dbb29bb50d0521e
anwesha-git <122990634+anwesha-git@users.noreply.github.com>
2023-07-01 16:45:33 +0200

    Update etl.py

--- etl.py
+++ etl.py
@@ -2,19 +2,19 @@ import configparser
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 
-
+#the function to load data from S3 to staging tables
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
-
+#the function to load data from staging tables to main (fact and dim) tables
 def insert_tables(cur, conn):
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
-
+#the main function assigning values and calling all functions
 def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
