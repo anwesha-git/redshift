6ccb6a7973e90e8e72719ac84db0eaf2bb3cd60c
anwesha-git <122990634+anwesha-git@users.noreply.github.com>
2023-07-01 16:47:28 +0200

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -2,19 +2,19 @@ import configparser
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
-
+#the function to drop any existing table with the same name, before creating the table
 def drop_tables(cur, conn):
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
-
+#the function to create staging and main tables
 def create_tables(cur, conn):
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
-
+#the main function assigning values and calling all functions
 def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
